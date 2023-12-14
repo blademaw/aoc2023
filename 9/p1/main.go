63461e13ceb3c7c10b71afdd63f893d32f330596
blademaw <jackoli3@me.com>
2023-12-14 23:57:50 +0800

    Add day 9 p1 solution

---
+++ 9/p1/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"aoc2023/utils"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func allZeros(ns []int) bool {
+	for _, n := range ns {
+		if n != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func computeDiffs(ns []int) []int {
+	diffArr := make([]int, len(ns)-1)
+	for i := 1; i < len(ns); i++ {
+		diffArr[i-1] = ns[i] - ns[i-1]
+	}
+
+	return diffArr
+}
+
+func nextRes(ns []int) int {
+	diffs := make([][]int, len(ns))
+	diffs[0] = ns
+
+	// Compute diffs until 0
+	for i := 1; i < len(ns); i++ {
+		if allZeros(diffs[i-1]) {
+			break
+		}
+
+		diffs[i] = computeDiffs(diffs[i-1])
+	}
+
+	// Sum the last column once finished for the new result
+	i, sum := 0, 0
+	for len(diffs[i]) > 0 && i < len(diffs) {
+		sum += diffs[i][len(diffs[i])-1]
+		i++
+	}
+
+	return sum
+}
+
+func main() {
+	file := flag.String("file", "data.txt", "the file of OASIS records.")
+	flag.Parse()
+
+	lines, _ := utils.ReadLines(*file)
+
+	sum := 0
+	for _, line := range lines {
+		sNums := strings.Split(line, " ")
+
+		ns := make([]int, len(sNums))
+		for i, s := range sNums {
+			ns[i], _ = strconv.Atoi(s)
+		}
+
+		sum += nextRes(ns)
+	}
+
+	fmt.Println(sum)
+}
